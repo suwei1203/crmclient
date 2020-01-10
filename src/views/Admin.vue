31049a24858e9050ce78b55bc0bd30bc411e86f6
suwei1203 <59653446+suwei1203@users.noreply.github.com>
2020-01-10 22:27:41 +0800

    Update Admin.vue

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,12 +1,99 @@
 <template>
 	<div>
-		admin
-		<router-view></router-view>
-	</div>	
+		<el-container>
+			<el-header>xx公司客户关系管理系统</el-header>
+			<el-container>
+				<el-aside width="200px">
+					<el-menu router default-active="2" class="el-menu-vertical-demo" background-color="#D3DCE6" text-color="#333"
+					 active-text-color="#777">
+						<el-submenu index="1">
+							<template slot="title">							
+								<span>营销管理</span>
+							</template>
+							<el-menu-item index="/admin/salechancelist">销售机会管理</el-menu-item>
+							<el-menu-item index="/admin/newsList">客户开发计划</el-menu-item>
+						</el-submenu>
+						<el-submenu index="2">
+							<template slot="title">
+								<span>客户管理</span>
+							</template>
+							<el-menu-item index="2-1">客户信息管理</el-menu-item>
+							<el-menu-item index="2-2">客户流失管理</el-menu-item>
+						</el-submenu>
+					</el-menu>
+				</el-aside>
+				<el-main id="main">
+					<router-view></router-view>
+				</el-main>
+			</el-container>
+			<el-footer>&copy; 东软睿道版权所有 www.neusoft.com</el-footer>
+		</el-container>
+	</div>
 </template>
 
 <script>
+	export default {
+		name: 'Admin',
+		data() {
+			return {
+				bodyHeight: 0
+			}
+		},
+		mounted() {
+			document.getElementById('main').style.height = document.body.clientHeight - 120 + 'px';
+			this.bodyHeight = document.body.clientHeight;
+			//监听浏览器窗口尺寸改变事件
+			window.onresize = () => {
+				//获取body高度，赋值给this.bodyHeight
+				this.bodyHeight = document.body.clientHeight;
+			}
+		},
+		watch: {
+			bodyHeight: function() {
+				document.getElementById('main').style.height = this.bodyHeight - 120 + 'px';
+			}
+		},
+		methods: {
+
+		}
+	}
 </script>
 
 <style>
+	html,
+	body {
+		margin: 0;
+		padding: 0;
+		width: 100%;
+		/*body高度一定要设置成100%，否则body的高度始终由内容撑开。*/
+		height: 100%;
+	}
+
+	.el-header,
+	.el-footer {
+		background-color: #B3C0D1;
+		color: #333;
+		text-align: center;
+		line-height: 60px;
+	}
+
+	.el-header {
+		font-size: 24px;
+	}
+
+	.el-footer {
+		font-size: 14px;
+	}
+
+	.el-aside {
+		background-color: #D3DCE6;
+		color: #333;
+		text-align: center;
+		line-height: 200px;
+	}
+
+	.el-main {
+		background-color: #E9EEF3;
+		color: #333;
+	}
 </style>
