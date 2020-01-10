5e3ddf18400e74791481e9f87719c08deec83c24
suwei1203 <59653446+suwei1203@users.noreply.github.com>
2020-01-10 22:27:26 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import Admin from '../views/Admin.vue'
 import Login from '../views/Login.vue'
 import AddSaleChance from '../views/sale/AddSaleChance.vue'
 import UpdateSaleChance from '../views/sale/UpdateSaleChance.vue'
+import SaleChanceList from '../views/sale/SaleChanceList.vue'
 
 Vue.use(VueRouter)
 
@@ -26,14 +27,18 @@ const routes = [{
 			path: '/admin/',
 			name: 'HelloWorld',
 			component: HelloWorld
-		}, {
+		},{
 			path: '/admin/addsalechance',
 			name: 'AddSaleChance',
 			component: AddSaleChance
-		}, {
+		},{
 			path: '/admin/updatesalechance',
 			name: 'UpdateSaleChance',
 			component: UpdateSaleChance
+		},{
+			path: '/admin/salechancelist',
+			name: 'SaleChanceList',
+			component: SaleChanceList
 		}]
 	}
 ]
