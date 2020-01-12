50dfcbd87a2d38b7774217c72290320aaa7b6740
suwei1203 <59653446+suwei1203@users.noreply.github.com>
2020-01-12 22:31:30 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -3,9 +3,13 @@ import VueRouter from 'vue-router'
 import HelloWorld from '../components/HelloWorld.vue'
 import Admin from '../views/Admin.vue'
 import Login from '../views/Login.vue'
-import AddSaleChance from '../views/sale/AddSaleChance.vue'
+import InsertSaleChance from '../views/sale/InsertSaleChance.vue'
 import UpdateSaleChance from '../views/sale/UpdateSaleChance.vue'
 import SaleChanceList from '../views/sale/SaleChanceList.vue'
+import AppointSaleChance from '../views/sale/AppointSaleChance.vue'
+import SaleChanceExecuteList from '../views/sale/SaleChanceExecuteList.vue'
+import SalePlanFormulateView from '../views/sale/AppointSaleChance.vue'
+
 
 Vue.use(VueRouter)
 
@@ -24,22 +28,39 @@ const routes = [{
 		name: 'Admin',
 		component: Admin,
 		children: [{
-			path: '/admin/',
-			name: 'HelloWorld',
-			component: HelloWorld
-		},{
-			path: '/admin/addsalechance',
-			name: 'AddSaleChance',
-			component: AddSaleChance
-		},{
-			path: '/admin/updatesalechance',
-			name: 'UpdateSaleChance',
-			component: UpdateSaleChance
-		},{
-			path: '/admin/salechancelist',
-			name: 'SaleChanceList',
-			component: SaleChanceList
-		}]
+				path: '/admin/',
+				name: 'HelloWorld',
+				component: HelloWorld
+			}, {
+				path: '/admin/insertsalechance',
+				name: 'InsertSaleChance',
+				component: InsertSaleChance
+			}, {
+				path: '/admin/updatesalechance',
+				name: 'UpdateSaleChance',
+				component: UpdateSaleChance
+			},
+			{
+				path: '/admin/salechancelist',
+				name: 'SaleChanceList',
+				component: SaleChanceList
+			},
+			{
+				path: '/admin/appointsalechance',
+				name: 'AppointSaleChance',
+				component: AppointSaleChance
+			},{
+				path: '/admin/salechanceexecutelist',
+				name: 'SaleChanceExecuteList',
+				component: SaleChanceExecuteList
+			},
+			{
+				path: '/admin/saleplanformulateview',
+				name: 'SalePlanFormulateView',
+				component: SalePlanFormulateView
+			}
+			
+		]
 	}
 ]
 
