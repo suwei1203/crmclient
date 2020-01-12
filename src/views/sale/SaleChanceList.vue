537979e1878e499a0beba687b4a675565be8e69b
suwei1203 <59653446+suwei1203@users.noreply.github.com>
2020-01-12 22:31:58 +0800

    Update SaleChanceList.vue

--- src/views/sale/SaleChanceList.vue
+++ src/views/sale/SaleChanceList.vue
@@ -3,27 +3,34 @@
 		<el-breadcrumb separator-class="el-icon-arrow-right">
 			<el-breadcrumb-item>营销管理</el-breadcrumb-item>
 			<el-breadcrumb-item>销售机会管理</el-breadcrumb-item>
+			<el-breadcrumb-item>销售机列表</el-breadcrumb-item>
 		</el-breadcrumb>
-		<el-form :inline="true" :model="saleChance" class="demo-form-inline" size="small">
+		<el-form :inline="true" :model="params" class="demo-form-inline" size="small">
 			<el-form-item label="客户名称">
-				<el-input v-model="saleChance.chanceCustName"></el-input>
+				<el-input v-model="params.chanceCustName"></el-input>
 			</el-form-item>
 			<el-form-item label="概要">
-				<el-input v-model="saleChance.chanceTitle"></el-input>
+				<el-input v-model="params.chanceTitle"></el-input>
 			</el-form-item>
 			<el-form-item label="联系人">
-				<el-input v-model="saleChance.chanceLinkman"></el-input>
+				<el-input v-model="params.chanceLinkman"></el-input>
 			</el-form-item>
 			<el-form-item>
-				<el-button type="primary" @click="">查询</el-button>
+				<el-button type="primary" @click="selectSaleChanceByConditions()">查询</el-button>
+			</el-form-item>
+			<el-form-item>
+				<el-button type="warning" @click="clearConditions()">清空</el-button>
+			</el-form-item>
+			<el-form-item>
+				<el-button  type="success" @click="insertSaleChanceView()">新建</el-button>
 			</el-form-item>
 		</el-form>
 		<el-table :data="result.data" stripe style="width: 100%">
-			<el-table-column prop="chanceId" label="编号" width="180">
+			<el-table-column prop="chanceId" label="编号" width="180">			
 			</el-table-column>
-			<el-table-column prop="chanceCustName" label="客户名称" width="180">
+			<el-table-column prop="chanceCustName" label="客户名称" width="250" >
 			</el-table-column>
-			<el-table-column prop="chanceTitle" label="概要">
+			<el-table-column prop="chanceTitle" label="概要" width="300">
 			</el-table-column>
 			<el-table-column prop="chanceLinkman" label="联系人">
 			</el-table-column>
@@ -33,12 +40,14 @@
 			</el-table-column>
 			<el-table-column fixed="right" label="操作" width="100">
 				<template slot-scope="scope">
-					<el-button class="el-icon-edit" type="text" size="medium"></el-button>
-					<el-button class="el-icon-delete" @click="handleClick(scope.row)" type="text" size="small"></el-button>
+					<el-button v-show="scope.row.chanceCreateId==userId" class="el-icon-edit" title="修改" @click="updateSaleChanceView(scope.row.chanceId)" type="text" size="medium"></el-button>
+					<el-button v-show="scope.row.chanceCreateId==userId" class="el-icon-delete"  title="删除" @click="delSaleChance(scope.row.chanceId)" type="text" size="small"></el-button>
+					<el-button v-show="userRoleId==2" class="el-icon-thumb" title="指派" @click="appointSaleChanceView(scope.row.chanceId,scope.row.chanceCreateId)" type="text" size="small"></el-button>
 				</template>
 			</el-table-column>
 		</el-table>
-		<el-pagination background layout="prev, pager, next" :total="1000">
+		<el-pagination background layout="prev, pager, next,jumper" :total="result.totalRow" :page-size="$store.state.maxPageNum"
+		 @current-change="handleCurrentChange">
 		</el-pagination>
 	</div>
 </template>
@@ -47,45 +56,71 @@
 	export default {
 		data() {
 			return {
-				saleChance: {
-					chanceId: 1,
-					chanceSource: '哪来的',
-					chanceCustName: '什么名',
-					chanceTitle: '标题',
-					chanceRate: 0,
-					chanceLinkman: '联系谁',
-					chanceTel: 15840010982,
-					chanceDesc: '描述机会',
-					chanceCreateId: 10,
-					chanceCreateDate: '',
-					//以下字段先不用
-					chanceDueId: 0,
-					chanceDueDate: '',
-					chanceStatus: 0
-				},
-				result:'',
+				result: '',
 				params: {
-					beginNum: 0,
-					maxPageNum: 8
-
-				}
-
-
+					chanceCustName: '',
+					chanceTitle: '',
+					chanceLinkman: '',
+					chanceStatus:0
+				},
+				userId: '',
+				userRoleId:''
 			}
 		},
 		created() {
 			this.fenye(1);
+			this.userId = this.$getSessionStorage("sysUser").userId;
+			this.userRoleId = this.$getSessionStorage("sysUser").userRoleId;
+			
 		},
 		methods: {
 			fenye(pageNum) {
-				this.$fenye('selectSaleChanceCount', 'selectSaleChancePaging', null, pageNum, this.$store.state.maxPageNum,
+				this.$fenye('selectSaleChanceCount', 'selectSaleChancePaging', this.params, pageNum, this.$store.state.maxPageNum,
 					(response) => {
-						console.log(response);
-						this.result=response;
+						this.result = response;
 					})
 			},
-			handleClick(){
-				
+			selectSaleChanceByConditions() {
+				this.fenye(1);
+			},
+			clearConditions() {
+				this.params.chanceCustName = '',
+					this.params.chanceTitle = '',
+					this.params.chanceLinkman = ''
+			},
+			insertSaleChanceView() {
+				this.$router.push("/admin/insertsalechance");
+			},
+			handleCurrentChange(val) {
+				this.fenye(val);
+			},
+			updateSaleChanceView(chanceId){
+				this.$setSessionStorage("chanceId",chanceId);
+				this.$router.push('UpdateSaleChance');
+			},							 
+			delSaleChance(chanceId) {
+				if (!confirm('确认删除？')) {
+					return;
+				}
+				this.$axios.post('delSaleChance', {
+						chanceId: chanceId
+					})
+					.then((response) => {
+						if (response.data == 1) {
+							console.log("删除成功");
+							this.fenye(1);
+						} else {
+							alert('删除失败');
+						}
+					})
+					.catch((error) => {
+						console.log(error);
+					})
+			},
+			appointSaleChanceView(chanceId,chanceCreateId){
+				this.$router.push("/admin/appointsalechance");
+				this.$setSessionStorage("chanceId",chanceId);
+				this.$setSessionStorage("chanceCreateId",chanceCreateId);
 			}
 		}
 
@@ -94,4 +129,8 @@
 </script>
 
 <style>
+	.demo-form-inline {
+		margin-top: 15px;
+		margin-bottom: 10px;
+	}
 </style>
