be5d074043b504258cb7391316a3d0b1f254c01c
suwei1203 <59653446+suwei1203@users.noreply.github.com>
2020-01-14 23:13:54 +0800

    Update SalePlanFormulateView.vue

--- src/views/sale/SalePlanFormulateView.vue
+++ src/views/sale/SalePlanFormulateView.vue
@@ -2,52 +2,52 @@
 	<div>
 		<el-breadcrumb separator-class="el-icon-arrow-right">
 			<el-breadcrumb-item>营销管理</el-breadcrumb-item>
-			<el-breadcrumb-item>客户开发计划</el-breadcrumb-item>
+			<el-breadcrumb-item :to="{ path: '/admin/salechancedeveloplist'}">客户开发计划</el-breadcrumb-item>
 			<el-breadcrumb-item>制定开发计划</el-breadcrumb-item>
 		</el-breadcrumb>
 		<el-button v-show="isExecute()" id="btn" type="primary" size="small" @click="salePlanExecuteView(saleChance.chanceId)">执行开发计划</el-button>
-
-		<el-form :inline="true" :model="saleChance" label-width="130px" style="margin-top: 15px;">
+		<el-form :inline="true" :model="saleChance" label-width="130px" style="margin-top: 15px;" disabled>
 			<el-form-item label="销售机会编号">
-				<el-input v-model="saleChance.chanceId" disabled></el-input>
+				<el-input v-model="saleChance.chanceId"></el-input>
 			</el-form-item>
 			<el-form-item label="机会来源">
-				<el-input v-model="saleChance.chanceSource" disabled></el-input>
+				<el-input v-model="saleChance.chanceSource"></el-input>
 			</el-form-item>
 			<el-form-item label="客户名称">
-				<el-input v-model="saleChance.chanceCustName" disabled></el-input>
+				<el-input v-model="saleChance.chanceCustName"></el-input>
 			</el-form-item>
 			<el-form-item label="成功概率(%)">
-				<el-input v-model="saleChance.chanceRate" disabled></el-input>
+				<el-input v-model="saleChance.chanceRate"></el-input>
 			</el-form-item>
 			<el-form-item label="概要">
-				<el-input v-model="saleChance.chanceTitle" disabled></el-input>
+				<el-input v-model="saleChance.chanceTitle"></el-input>
 			</el-form-item>
 			<el-form-item label="联系人">
-				<el-input v-model="saleChance.chanceLinkman" disabled></el-input>
+				<el-input v-model="saleChance.chanceLinkman"></el-input>
 			</el-form-item>
 			<el-form-item label="联系人电话">
-				<el-input v-model="saleChance.chanceTel" disabled></el-input>
+				<el-input v-model="saleChance.chanceTel"></el-input>
 			</el-form-item>
 			<el-form-item label="创建人">
-				<el-input v-model="userName" disabled></el-input>
+				<el-input v-model="userName"></el-input>
 			</el-form-item>
 			<el-form-item label="创建时间">
-				<el-input v-model="saleChance.chanceCreateDate" disabled></el-input>
+				<el-input v-model="saleChance.chanceCreateDate"></el-input>
 			</el-form-item>
 			<el-form-item label="机会描述">
-				<el-input type="textarea" v-model="saleChance.chanceDesc" style="width:400px;" disabled></el-input>
+				<el-input type="textarea" v-model="saleChance.chanceDesc" style="width:400px;"></el-input>
 			</el-form-item>
 			<el-form-item label="指派给" required>
-				<el-input v-model="userName" style="width:400px;" disabled></el-input>
+				<el-input v-model="userName" style="width:400px;"></el-input>
 			</el-form-item>
 			<el-form-item label="指派日期">
-				<el-input v-model="saleChance.chanceDueDate" disabled></el-input>
+				<el-input v-model="saleChance.chanceDueDate"></el-input>
 			</el-form-item>
 		</el-form>
-		
-		<el-form :model="salePlan" label-width="90px" style="margin-top: 15px; width:2000px;">
+
+		<el-form :model="salePlan" label-width="130px" style="margin-top: 15px;">
 			<el-form-item label="已有计划" v-for="(item, index) in salePlan.list" :key="index">
+				<!-- @change input内容改变时触发 传入index 唯一性-->
 				<el-input v-model="item.planTodo" @change="changeContent(index)"></el-input>
 				<el-button v-show="numberarr.indexOf(index)!=-1" style="margin-left:40px;" @click="updateSalePlan(item,index)">保存</el-button>
 				<el-button @click="delSalePlan(item)" style="margin-left:40px;">删除</el-button>
@@ -80,105 +80,82 @@
 					chanceStatus: 0
 				},
 				//用于显示指派人名字
-				userName:'' ,
+				userName: '',
 				//已保存的销售计划
 				salePlan: {
 					list: []
 				},
-				//用于更改已存在的销售机会内容时  动态显示保存按钮
-				numberarr:[],
-					
+				//用于动态显示保存按钮
+				numberarr: [],
+				//存放新建的销售计划
 				newSalePlan: {
 					planChcId: 0,
 					planTodo: '',
 					planResult: ''
 				},
-				//此数据用于刷新组件
-				update: true
 			};
 		},
 		created() {
-			this.userName=this.$getSessionStorage("sysUser").userName;
-			console.log(this.userName);
+			this.userName = this.$getSessionStorage("sysUser").userName;
 			this.newSalePlan.planChcId = this.$getSessionStorage("chanceId");
+			// 获取当前销售机会的信息
 			this.$axios.post('selectSaleChanceByChanceId', {
 					chanceId: this.$getSessionStorage("chanceId")
 				})
 				.then((response) => {
-
-					console.log(response);
 					this.saleChance = response.data;
 				})
 				.catch((error) => {
 					console.log(error);
 				})
-			// 机会id下的所有计划 待修改
-			this.$axios.post('selectSalePlanByPlanChcId', {
-					planChcId: this.$getSessionStorage("chanceId")
-				})
-				.then((response) => {
-					this.salePlan.list = response.data;
-				})
-				.catch((error) => {
-					console.log(error);
-				})
-
+			// 销售机会下的所有计划
+			this.getSalePlan();
 		},
-	
+
 		methods: {
+			//封装 获取销售计划的方法
+			getSalePlan() {
+				this.$axios.post('selectSalePlanByPlanChcId', {
+						planChcId: this.$getSessionStorage("chanceId")
+					})
+					.then((response) => {
+						this.salePlan.list = response.data;
+					})
+					.catch((error) => {
+						console.log(error);
+					})
+			},
 			//是否有执行计划权限
-			isExecute(){
-				return this.saleChance.chanceDueId==this.$getSessionStorage('sysUser').userId;
+			isExecute() {
+				return this.saleChance.chanceDueId == this.$getSessionStorage('sysUser').userId;
 			},
 			//跳转到执行计划组件
 			salePlanExecuteView(chanceId) {
-				this.$setSessionStorage('chanceId',chanceId);
+				this.$setSessionStorage('chanceId', chanceId);
 				this.$router.push('/admin/saleplanexecuteview');
 			},
-			
-			
-			
-			reload() {
-				// 移除组件
-				this.update = false
-				// 在组件移除后，重新渲染组件
-				// this.$nextTick可实现在DOM 状态更新后，执行传入的方法。
-				this.$nextTick(() => {
-					this.update = true
-				})
-			},
-			//已存在的销售机会内容改变时触发 
-			changeContent(index){
+			//已保存的销售机会内容改变时触发 
+			changeContent(index) {
 				this.numberarr.push(index);
 			},
-			removenum(arr,num){
-				for(let i=0;i<arr.length;i++){
-					if(arr[i]==num){
-						arr.splice(i,1); 
+			//将数值num从arr数组中移除
+			removenum(arr, num) {
+				for (let i = 0; i < arr.length; i++) {
+					if (arr[i] == num) {
+						arr.splice(i, 1);
 					}
 				}
 			},
-			
+			//添加销售计划
 			insertSalePlan() {
 				this.$axios.post('insertSalePlan', this.newSalePlan)
 					.then((response) => {
 						if (response.data == 1) {
 							alert('添加成功');
-
-							//重新得到数据
-							this.$axios.post('selectSalePlanByPlanChcId', {
-									planChcId: this.$getSessionStorage("chanceId")
-								})
-								.then((response) => {
-									this.salePlan.list = response.data;
-								})
-								.catch((error) => {
-									console.log(error);
-								})
-
-							this.newSalePlan.planTodo = '',
-
-								this.reload();
+							//重新得到销售计划数据
+							this.getSalePlan();
+							//清空新建销售计划input控件中内容
+							this.newSalePlan.planTodo = '';
 						} else {
 							alert('添加失败');
 						}
@@ -187,26 +164,16 @@
 						console.log(error);
 					})
 			},
-			
-			updateSalePlan(item,index){
-				this.$axios.post('updateSalePlan',item)
+			// 修改销售计划
+			updateSalePlan(item, index) {
+				this.$axios.post('updateSalePlan', item)
 					.then((response) => {
 						if (response.data == 1) {
-							console.log(this.numberarr);
 							alert('修改成功');
-							this.removenum(this.numberarr,index);
-							console.log("---");
-							console.log(this.numberarr);
-							//重新得到数据
-							this.$axios.post('selectSalePlanByPlanChcId', {
-									planChcId: this.$getSessionStorage("chanceId")
-								})
-								.then((response) => {
-									this.salePlan.list = response.data;
-								})
-								.catch((error) => {
-									console.log(error);
-								})
+							// 保存按钮消失
+							this.removenum(this.numberarr, index);
+							//重新得到销售计划数据
+							this.getSalePlan();
 						} else {
 							alert('修改失败');
 						}
@@ -215,27 +182,17 @@
 						console.log(error);
 					})
 			},
-			
+			//删除销售计划
 			delSalePlan(item) {
 				if (!confirm('确认删除？')) {
 					return;
 				}
-				this.$axios.post('delSalePlan',item)
+				this.$axios.post('delSalePlan', item)
 					.then((response) => {
 						if (response.data == 1) {
 							console.log("删除成功");
-							
-							//重新得到数据
-							this.$axios.post('selectSalePlanByPlanChcId', {
-									planChcId: this.$getSessionStorage("chanceId")
-								})
-								.then((response) => {
-									this.salePlan.list = response.data;
-								})
-								.catch((error) => {
-									console.log(error);
-								})
-							
+							//重新得到销售计划数据
+							this.getSalePlan();
 						} else {
 							alert('删除失败');
 						}
